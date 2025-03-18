4d60940a725d1f67309fa599045808150b91f66f
vitalii boiko <vukelan@gmail.com>
2025-03-18 22:37:56 +0200

    add nestjs mongoos

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LessonService } from './lesson.service';
 import { LessonController } from './lesson.controller';
+import { Lesson, LessonSchema } from './lesson.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Lesson.name, schema: LessonSchema }]),
+  ],
   controllers: [LessonController],
   providers: [LessonService],
 })
