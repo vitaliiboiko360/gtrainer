8b32eb1785aae0742399f6d991a26283711a10f2
vitalii boiko <vukelan@gmail.com>
2025-03-18 22:38:19 +0200

    add lesson doc schema

---
+++ src/lesson/lesson.schema.ts
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type LessonDocument = HydratedDocument<Lesson>;
+
+type LineObject = { text: string; start: number; end: number };
+
+@Schema()
+export class Lesson {
+  @Prop()
+  audio: string;
+
+  @Prop()
+  lines: Array<LineObject>;
+}
+
+export const LessonSchema = SchemaFactory.createForClass(Lesson);
+
+export const LessonSchema2 = new mongoose.Schema({
+  name: String,
+  age: Number,
+  breed: String,
+});
