b63014441ab07ccb909d211365a1b44b3558a013
vitalii boiko <vukelan@gmail.com>
2025-03-19 19:24:56 +0200

    correct import into module

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { LessonService } from './lesson.service';
 import { LessonController } from './lesson.controller';
-import { Lesson, LessonSchema } from './lesson.schema';
 import { lessonsProviders } from './lesson.provider';
 import { DatabaseModule } from 'src/db/database.module';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    MongooseModule.forFeature([{ name: Lesson.name, schema: LessonSchema }]),
-  ],
+  imports: [DatabaseModule],
   controllers: [LessonController],
   providers: [LessonService, ...lessonsProviders],
 })
