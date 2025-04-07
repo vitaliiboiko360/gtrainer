e25511011db8e16b4ed1992119cff285972b73ce
vitalii boiko <vukelan@gmail.com>
2025-04-07 03:45:02 +0300

    update schema

--- src/lesson/lesson.schema.ts
+++ src/lesson/lesson.schema.ts
@@ -5,10 +5,26 @@ import { HydratedDocument } from 'mongoose';
 
 export type LessonDocument = HydratedDocument<Lesson>;
 
-export type LineObject = { text: string; start: number; end: number };
+export type Translation = {
+  indexInSentence: number;
+  wordInEnglish: string;
+  partOfSpeech: number;
+};
+
+export type LineObject = {
+  text: string;
+  start: number;
+  end: number;
+  orderNumber: number;
+  speechParts: Array<number>;
+  translations: Array<Translation>;
+};
 
 @Schema()
 export class Lesson {
+  @Prop()
+  name: string;
+
   @Prop()
   audio: string;
 
