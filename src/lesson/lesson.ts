ada8b1689428d5c66b43fe7a08acb6a8d4df68e1
vitalii boiko <vukelan@gmail.com>
2025-04-18 01:05:08 +0300

    move out lesson into separate file

---
+++ src/lesson/lesson.ts
@@ -0,0 +1,22 @@
+export type Translation = {
+  indexInSentence: number;
+  wordInEnglish: string;
+  partOfSpeech: number;
+};
+
+export type LineObject = {
+  text: string;
+  start: number;
+  end: number;
+  wordCount: number;
+  lineNumber: number;
+  speechParts: Array<number>;
+  translations: Array<Translation>;
+};
+
+export class LessonBase {
+  _id: string;
+  name: string;
+  audio: string;
+  lines: Array<LineObject>;
+}
