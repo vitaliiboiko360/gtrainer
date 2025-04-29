d3cca6770f3e0db03b1f40ea0cd0ec43807b7079
vitalii boiko <vukelan@gmail.com>
2025-04-30 00:07:53 +0300

    rename lesson interface

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -2,21 +2,21 @@ import { Model } from 'mongoose';
 import { Injectable, Inject } from '@nestjs/common';
 // import { CreateLessonDto } from './dto/create-lesson.dto';
 import { UpdateLessonDto } from './dto/update-lesson.dto';
-import { Lesson } from './lesson.interface';
+import { I_Lesson } from './lesson.interface';
 import { LESSON_MODEL } from './constants';
 
 @Injectable()
 export class LessonService {
   constructor(
     @Inject(LESSON_MODEL)
-    private lessonModel: Model<Lesson>,
+    private lessonModel: Model<I_Lesson>,
   ) {}
 
   // create(createLessonDto: CreateLessonDto) {
   //   return 'This action adds a new lesson';
   // }
 
-  findAll(): Promise<Lesson[]> {
+  findAll(): Promise<I_Lesson[]> {
     return this.lessonModel.find().exec();
   }
 
@@ -24,7 +24,7 @@ export class LessonService {
   //   return `This action returns a #${id} lesson`;
   // }
 
-  findOne(name: string) {
+  findOne(name: string): Promise<I_Lesson | null> {
     return this.lessonModel.findOne({ name: name }).exec();
   }
 
