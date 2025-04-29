c9585836898ccb7ef6604776c7bfd795e4a532bb
vitalii boiko <vukelan@gmail.com>
2025-04-30 00:06:32 +0300

    add translation service

--- src/translation/translation.service.ts
+++ src/translation/translation.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTranslationDto } from './dto/create-translation.dto';
-import { UpdateTranslationDto } from './dto/update-translation.dto';
+import { Injectable, Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { I_Translation } from './translation.interface';
+import { TRANSLATION_MODEL } from './constants';
 
 @Injectable()
 export class TranslationService {
-  create(createTranslationDto: CreateTranslationDto) {
-    return 'This action adds a new translation';
+  constructor(
+    @Inject(TRANSLATION_MODEL)
+    private translationService: Model<I_Translation>,
+  ) {}
+  // create(createTranslationDto: CreateTranslationDto) {
+  //   return 'This action adds a new translation';
+  // }
+  findAll(): Promise<I_Translation[]> {
+    return this.translationService.find().exec();
+  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} translation`;
+  // }
+
+  findOne(name: string): Promise<I_Translation | null> {
+    return this.translationService.findOne({ name: name }).exec();
   }
 
-  findAll() {
-    return `This action returns all translation`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} translation`;
-  }
-
-  update(id: number, updateTranslationDto: UpdateTranslationDto) {
-    return `This action updates a #${id} translation`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} translation`;
-  }
+  // update(id: number, updateTranslationDto: UpdateTranslationDto) {
+  //   return `This action updates a #${id} translation`;
+  // }
+  // remove(id: number) {
+  //   return `This action removes a #${id} translation`;
+  // }
 }
