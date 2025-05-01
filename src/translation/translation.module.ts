f748b5a1b5f3c440e5188441e8349434bb8c15ee
user1 <vukelan@gmail.com>
2025-05-01 23:59:22 +0300

    add translation service to module

--- src/translation/translation.module.ts
+++ src/translation/translation.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TranslationService } from './translation.service';
 import { TranslationController } from './translation.controller';
+import { translationsProviders } from './translation.provider';
+import { DatabaseModule } from 'src/db/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TranslationController],
-  providers: [TranslationService],
+  providers: [TranslationService, ...translationsProviders],
 })
 export class TranslationModule {
   constructor(private translationService: TranslationService) {}
